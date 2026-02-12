455adb2ffcdb4b2e163dd7acdab37a0eeb9d6065
devstar3504-pixel <devstar3504@gcplab.me>
2026-02-12 11:28:55 +0530

    Update login.py

--- login.py
+++ login.py
@@ -4,3 +4,7 @@ def login(user, pwd):
     conn = sqlite3.connect("users.db")
     query = f"SELECT * FROM users WHERE username='{user}' AND password='{pwd}'"
     return conn.execute(query).fetchone()
+
+
+def unsafe_query(q):
+    return f"SELECT * FROM data WHERE id = {q}"
